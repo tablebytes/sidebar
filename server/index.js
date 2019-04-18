3a8f7ef740c98dc751ed8defa21fa02b8190007c
Theresa <tli2@cca.com>
2019-04-18 15:21:22 -0700

    Implement post and put requests

--- server/index.js
+++ server/index.js
@@ -10,7 +10,34 @@ const app = express();
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use('/restaurants/:id', express.static(path.join(__dirname, '/../client/dist')));
+
+app.post('/api/restaurants/info/new', (req, res) => {
+  SidebarInfo.model.find({ restaurantId: req.body.restaurantId })
+  .then(info => {
+    if (info.length > 0) {
+      console.log('Restaurant id is taken.');
+    } else {
+      SidebarInfo.model.create(req.body)
+        .then(() => console.log('Successly added new restaurant information!'))
+        .catch((error) => console.log(error));
+    }
+  });
+});
+
+app.post('/api/restaurants/overview/new', (req, res) => {
+  Overview.model.find({ restaurantId: req.body.restaurantId })
+  .then(info => {
+    if (info.length > 0) {
+      console.log('Restaurant id is taken.');
+    } else {
+      Overview.model.create(req.body)
+        .then(() => console.log('Successly added new restaurant overview!'))
+        .catch((error) => console.log(error));
+    }
+  });
+});
 
 app.get('/api/restaurants/:id/info', (req, res) => {
   SidebarInfo.getSidebarInfo(req.params.id)
@@ -28,7 +55,34 @@ app.get('/api/restaurants/:id/overview', (req, res) => {
           res.send(overview);
         })
     })
-})
+});
+
+app.put('/api/restaurants/:id/info/update', (req, res) => {
+  SidebarInfo.model.updateOne({ restaurantId: req.params.id}, req.body)
+    .then(() => console.log('Sucessfully updated restaurant information.'))
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+app.put('/api/restaurants/:id/overview/update', (req, res) => {
+  Overview.model.find({ restaurantId: req.body.restaurantId })
+  .then(info => {
+    if (info.length > 0) {
+      console.log('Restaurant does not exist.');
+    } else {
+      Overview.model.updateOne({ restaurantId: req.params.id}, req.body)
+        .then(() => console.log('Sucessfully updated restaurant overview.'))
+        .catch(err => {
+          console.log(err);
+        });
+    }
+  });
+});
+
+// Delete
+  // Overview
+  // Sidebar
 
 const server = app.listen(port, () => {
   console.log(`Now listening on port ${port}`);
