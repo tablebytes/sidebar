88719e13b2e82d057aee1d9fda1c51900ec42437
Theresa <tli2@cca.com>
2019-04-23 11:42:25 -0700

    Implement write to file

--- database/dataGenerator.js
+++ database/dataGenerator.js
@@ -1,14 +1,10 @@
 const faker = require('faker');
 const fs = require('fs');
 
-const sampleSidebarItems = [];
-const sampleOverviewItems = [];
-
 const totalItems = 10000000;
+const randRange = (min, max) => (Math.floor(Math.random() * (max + 1 - min)) + min);
 
-const populateItems = () => {
-  const randRange = (min, max) => (Math.floor(Math.random() * (max + 1 - min)) + min);
-  for (let restaurantId = 1; restaurantId <= totalItems; restaurantId += 1) {
+const populateItems = (restaurantId) => {
     let newItem = {};
 
     newItem.restaurantId = restaurantId;
@@ -29,7 +25,7 @@ const populateItems = () => {
     newItem.phone = faker.phone.phoneNumber();
     newItem.website = faker.internet.url();
     newItem.payment = 'Visa, Discover, MasterCard';
-    newItem.dress = [faker.commerce.productAdjective(), faker.commerce.productMaterial()].join(' ');
+    newItem.dress = `${faker.commerce.productAdjective()} ${faker.commerce.productMaterial()}`;
     newItem.chef = faker.name.findName();
 
     if (randRange(0, 2) === 2) {
@@ -38,8 +34,6 @@ const populateItems = () => {
     if (randRange(0, 2) === 2) {
       newItem.privateFacilities = faker.lorem.sentences();
     }
-
-    sampleSidebarItems.push(newItem); 
 
     let newOverviewItem = {};
 
@@ -50,7 +44,7 @@ const populateItems = () => {
     const minRange = Math.floor(Math.random() * 42) + 8;
     const maxRange = minRange + Math.floor(Math.random() * 10) + 5
     newOverviewItem.costRange = [minRange, maxRange];
-    newOverviewItem.cuisines = newItem.cuisines[0];
+    newOverviewItem.cuisines = cuisines[0];
     newOverviewItem.description = faker.lorem.paragraph();
     const tagCount = randRange(1, 3);
     let tags = [];
@@ -62,41 +56,47 @@ const populateItems = () => {
     }
     newOverviewItem.tags = tags;
 
-    sampleOverviewItems.push(newOverviewItem);
-  }
+    return [newItem, newOverviewItem];
 }
 
-populateItems();
-
-let sidebarData = sampleSidebarItems.map((data) => {
+const stringifyItem = (data) => {
   return Object.values(data).map((data) => JSON.stringify(data)).join(',') + '\n';
-});
-
-let overviewData = sampleOverviewItems.map((data) => {
-  return Object.values(data).map((data) => JSON.stringify(data)).join(',') + '\n';
-});
+}
 
 const sidebarWriter = fs.createWriteStream('sidebar.csv');
 
 const overviewWriter = fs.createWriteStream('overview.csv');
 
-const writeToFile = (data, writer) => {
+const writeToFile = () => {
   let i = 0;
-  let notFull = true;
+  let sidebarCanWrite = true;
+  let overviewCanWrite = true;
+  
+  let data;
   const write = () => {
-    while (i < totalItems & notFull) {
-      notFull = writer.write(data[i]);
+    if (!sidebarCanWrite) {
+      overviewCanWrite = overviewWriter.write(stringifyItem(data[1]));
       i += 1;
     }
+    sidebarCanWrite = true;
+    overviewCanWrite = true;
+    while (i < totalItems & sidebarCanWrite & overviewCanWrite) {
+      data = populateItems(i + 1);
+      sidebarCanWrite = sidebarWriter.write(stringifyItem(data[0]));
+      if (sidebarCanWrite) {
+        overviewCanWrite = overviewWriter.write(stringifyItem(data[1]));
+        i += 1;
+      }
+    }
     if (i < totalItems) {
-      writer.on('drain', write);
+      if (overviewCanWrite) {
+        sidebarWriter.once('drain', write);
+      } else {
+        overviewWriter.once('drain', write);
+      }
     }
   }
   write();
 }
 
-writeToFile(sidebarData, sidebarWriter);
-writeToFile(overviewData, overviewWriter);
-
-// exports.sampleSidebarItems = sampleSidebarItems;
-// exports.sampleOverviewItems = sampleOverviewItems;
+writeToFile();
