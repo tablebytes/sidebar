11025b2e62efaf0faa09e489b334ff58573d191f
Theresa <tli2@cca.com>
2019-05-02 20:19:02 -0700

    Reformat to be more likely to generate recent items

--- my-function.js
+++ my-function.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const generateRandomNumber = (userContext, events, done) => {
-  userContext.vars.id = Math.floor(Math.random() * 10000000);
+  userContext.vars.id = Math.floor(Math.random() * 10) < 8 ? Math.floor(Math.random() * 1000000) + 9000000 :  Math.floor(Math.random() * 10000000);
 
   return done();
 };
